4f5773662aa4c1107a110727f2e72f27075b9002
AdrianLeon51 <65872008+AdrianLeon51@users.noreply.github.com>
2021-03-12 10:58:24 -0500

    Add files via upload

--- spc-script.py
+++ spc-script.py
@@ -9,13 +9,13 @@ import os
 def listFiles(path, extension):
     return [f for f in os.listdir(path) if f.endswith(extension)]
 
-path='/home/adrianl/Documents/Semester 10/Tesis Data/IFTR'
+inputpath='/home/adrianl/Documents/Semester 10/Tesis Data/IFTR'
 ext = '.spc'
-files = listFiles(path, ext)
-
+files = listFiles(inputpath, ext)
+outputpath='/Filestxt/'
 
 for f in files:
 	d_name,d_ext=os.path.splitext(f)
 	d = spc.File(str(f))
 	d.data_txt()
-	d.write_file(path+'/Filestxt/'+d_name+'.txt')
+	d.write_file(path+outputpath+d_name+'.txt')
